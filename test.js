bb697e83aee75b263633ce48d6bd9b7f9ed56d1f
Jannis R <mail@jannisr.de>
2018-03-10 20:17:13 +0100

    add tests for encode & solveUnsafe :white_check_mark:

--- test.js
+++ test.js
@@ -5,13 +5,54 @@ const solve = require(".")
 const {encode, solveUnsafe, SATISFIABLE, UNSATISFIABLE} = solve
 
 const formula1 = [
+  ['!A', 'B', 'C'],
+  ['!B', 'C'],
+  ['!C', 'D'],
+  ['!D', 'A', 'B', 'C']
+]
+const assumptions1 = ['A', '!C']
+
+const formula2 = [
   ['A', 'B'],
   ['!A', 'B']
 ]
-const assumptions1 = ['!A', 'B']
+const assumptions2 = ['!A', 'B']
+
+test('encode works', (t) => {
+  const [encFormula, encAssumptions] = encode(formula1, assumptions1)
+
+  const expectedFormula = Buffer.from([
+    256 - 1, 2, 3, 0,
+    256 - 2, 3, 0,
+    256 - 3, 4, 0,
+    256 - 4, 1, 2, 3, 0
+  ])
+  t.ok(Buffer.isBuffer(encFormula))
+  t.equal(encFormula.toString('hex'), expectedFormula.toString('hex'))
+
+  const expectedAssumptions = Buffer.from([1, 256 - 3])
+  t.ok(Buffer.isBuffer(encAssumptions))
+  t.equal(encAssumptions.toString('hex'), expectedAssumptions.toString('hex'))
+
+  t.end()
+})
+
+test('solveUnsafe works', (t) => {
+  // (¬A ∨ B)∧(¬B ∨ C)∧(¬C ∨ A)
+  const encFormula = Buffer.from([
+    256 - 1, 2, 0,
+    256 - 2, 3, 0,
+    256 - 3, 1, 0
+  ])
+  const encAssumptions = Buffer.from([])
+
+  const solution = solveUnsafe(encFormula, encAssumptions)
+  t.equal(solution[0], 10) // assert if satisfiable
+  t.end()
+})
 
 test('solve solves a simple problem', (t) => {
-  const {status, satisfiable} = solve(formula1)
+  const {status, satisfiable} = solve(formula2)
 
   t.equal(status, SATISFIABLE)
   t.equal(satisfiable, true)
@@ -19,7 +60,7 @@ test('solve solves a simple problem', (t) => {
 })
 
 test('solve supports assumptions', (t) => {
-  const {solution, status, satisfiable} = solve(formula1, assumptions1)
+  const {solution, status, satisfiable} = solve(formula2, assumptions2)
 
   t.equal(solution.length, 2)
   t.equal(solution[0], -1)

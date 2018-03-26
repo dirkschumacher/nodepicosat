ab801ab915a5f393b8c656c65ebc84a0bd27a53b
Jannis R <mail@jannisr.de>
2018-03-27 00:46:10 +0200

    adapt tests to fe9c3ef :white_check_mark:

--- test.js
+++ test.js
@@ -45,16 +45,16 @@ test('encodeInt32Array works', (t) => {
 test('encodeStrings works', (t) => {
   const [encFormula, encAssumptions] = encodeStrings(formula1, assumptions1)
 
-  const expectedFormula = Buffer.from([
-    256 - 1, 2, 3, 0,
-    256 - 2, 3, 0,
-    256 - 3, 4, 0,
-    256 - 4, 1, 2, 3, 0
+  const expectedFormula = encodeInt32Array([
+    -1, 2, 3, 0,
+    -2, 3, 0,
+    -3, 4, 0,
+    -4, 1, 2, 3, 0
   ])
   t.ok(Buffer.isBuffer(encFormula))
   t.equal(encFormula.toString('hex'), expectedFormula.toString('hex'))
 
-  const expectedAssumptions = Buffer.from([1, 256 - 3])
+  const expectedAssumptions = encodeInt32Array([1, -3])
   t.ok(Buffer.isBuffer(encAssumptions))
   t.equal(encAssumptions.toString('hex'), expectedAssumptions.toString('hex'))
 
@@ -69,14 +69,14 @@ test('encodeIntegers works', (t) => {
     -1 // !A
   ])
 
-  const expectedFormula = Buffer.from([
-    1, 2, 0,
-    256 - 1, 2, 0
+  const expectedFormula = encodeInt32Array([
+     1, 2, 0,
+    -1, 2, 0
   ])
   t.ok(Buffer.isBuffer(encFormula))
   t.equal(encFormula.toString('hex'), expectedFormula.toString('hex'))
 
-  const expectedAssumptions = Buffer.from([256 - 1])
+  const expectedAssumptions = encodeInt32Array([-1])
   t.ok(Buffer.isBuffer(encAssumptions))
   t.equal(encAssumptions.toString('hex'), expectedAssumptions.toString('hex'))
 
@@ -119,12 +119,12 @@ test('solve works with >255 variables', (t) => {
 test('solveUnsafe works', (t) => {
   // (¬A ∨ B)∧(¬B ∨ C)∧(¬C ∨ A)
   // A = 1, B = 2, C = 3
-  const encFormula = Buffer.from([
-    256 - 1, 2, 0,
-    256 - 2, 3, 0,
-    256 - 3, 1, 0
+  const encFormula = encodeInt32Array([
+    -1, 2, 0,
+    -2, 3, 0,
+    -3, 1, 0
   ])
-  const encAssumptions = Buffer.from([])
+  const encAssumptions = encodeInt32Array([])
 
   const solution = solveUnsafe(encFormula, encAssumptions)
   t.equal(solution[0], 10) // assert if satisfiable
